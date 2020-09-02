348914209771fd99f19cf7c22d9b5765b7077225
TNThacker2015 <tnthacker2015@gmail.com>
2020-09-02 11:34:36 -0700

    rewrite
    Signed-off-by: TNThacker2015 <tnthacker2015@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -5,33 +5,13 @@ import { createRandomInterval } from "./structures/timing";
 import { WebhookClient } from "discord.js";
 import { join } from "path";
 import _ from "lodash";
-const loadMarkov = (url: string) =>
-	new Markov(
-		(JSON.parse(fs.readFileSync(join(__dirname, url), { encoding: "utf8" })) as Message[])
-			.map(x => x.content)
-			.filter(Boolean),
-			"word", 2, false
-	);
+import { list } from "./list";
 const auth: {
 	webhook: {
 		id: string;
 		token: string;
 	};
-} = JSON.parse(fs.readFileSync(join(__dirname, "../auth.json"), { encoding: "utf8" }));
-const f: Message[] = JSON.parse(fs.readFileSync(join(__dirname, "../data/zef.json"), { encoding: "utf8" }))
-const kk=f.map(x=>new Date(x.timestamp).getHours()).reduce((l,c)=>{l[c]=l[c]?l[c]+1:1;return l}, {} as { [idnex: number]: number })
-for (let i = 0;i<24;i++)kk[i]?[]:kk[i]=0
-
-const zirqn = loadMarkov("../data/zirqn.json");
-const bow = loadMarkov("../data/bow.json");
-const florida = loadMarkov("../data/florida.json");
-const rceckie = loadMarkov("../data/rceckie.json");
-const rad = loadMarkov("../data/rad.json");
-const dae = loadMarkov("../data/dae.json");
-const zef = loadMarkov("../data/zef.json");
-const axo = loadMarkov("../data/axo.json");
-const ten = loadMarkov("../data/ten.json");
-const fin = loadMarkov("../data/fin.json");
+} = JSON.parse(fs.readFileSync(join(__dirname, "../auth.json"), { encoding: "utf8" })); /*
 if (process.argv[2] === "zirqn") {
 	for (let i = 0; i < 10; i++) console.log(zirqn.generate());
 	process.exit(0);
@@ -56,7 +36,7 @@ if (process.argv[2] === "zirqn") {
 	console.log("====")
 	for (const item of shuffled) console.log(`${item.content}\n-----`);
 	process.exit(0);
-}
+}*/
 const tags = {
 	"624744152550342666": "axo#4475",
 	"156922894415429632": "bowknee shape row#0389",
@@ -69,78 +49,26 @@ const tags = {
 	"320591753956687882": "Findex_#5603"
 }
 const webhook = new WebhookClient(auth.webhook.id, auth.webhook.token, { disableMentions: "all" });
-const createAutomate = (markov: Markov, username: string, avatarURL: string, min = 10000, max = 30000) =>
-	createRandomInterval(
+const createAutomate = (filename: string, username: string, avatarURL: string, min = 10000, max = 30000) =>
+	{
+		const arr = (JSON.parse(fs.readFileSync(join(__dirname, `../data/${filename}.json`), { encoding: "utf8" })) as Message[]);
+		const atts = arr.map(x => x.attachments);
+		const markov = new Markov(
+			arr.map(x => x.content)
+				.filter(Boolean),
+				"word", 2, false
+		);
+		createRandomInterval(
 		async () => {
+			const file = atts[Math.floor(Math.random() * atts.length)];
 			const generated = markov.generate();
 			await webhook.send(generated.replace(/\<\@\!?(\d+)\>/g, (x, n: keyof typeof tags) => tags[n] ? `@${tags[n]}` : x).slice(0, 1999), {
 				username,
 				avatarURL,
+				files: (Math.round(Math.random()) ? file[Math.floor(Math.random() * file.length)] : []) as string[]
 			});
 		},
 		min,
 		max
-	);
-createAutomate(
-	zirqn,
-	"furqn",
-	"https://cdn.discordapp.com/attachments/735321036471402507/744397698576416848/Untitled1352_20200815202904.png"
-	// "https://cdn.discordapp.com/avatars/437845189227446273/6905a41c1d827dbd3efc4f3ef997a956.png?size=1024"
-);
-createAutomate(
-	bow,
-	"bowknee shape row",
-	"https://cdn.discordapp.com/avatars/156922894415429632/baf192f377fec106aa19cf856a7ee5f1.png?size=2048"
-);
-createAutomate(
-	florida,
-	"FloridaCan",
-	"https://cdn.discordapp.com/avatars/678405690464665631/e8961af99ba6fb340a69089fb7c31bcb.png?size=2048"
-);
-createAutomate(
-	rad,
-	"Radeline",
-	"https://cdn.discordapp.com/avatars/630047851011112970/74d044bf2cc38610e241fd8cf187de48.png?size=2048",
-	2000,
-	50000
-);
-createAutomate(
-	dae,
-	"Daemon_Ignis",
-	"https://cdn.discordapp.com/avatars/321349427476299777/84c7f789be81a1e49bb248d295fd787a.png?size=2048",
-	3000,
-	60000
-);
-createAutomate(
-	zef,
-	"unexpected william ░",
-	"https://cdn.discordapp.com/avatars/413143886702313472/5795319df1a0ff89e1d4d491c9ad2879.webp?size=1024"
-);
-createAutomate(
-	fin,
-	"Findex_",
-	"https://cdn.discordapp.com/avatars/320591753956687882/a_ab412847d943c78188d595c5f50dc04c.gif?size=2048"
-);
-createAutomate(
-	axo,
-	"axo",
-	"https://cdn.discordapp.com/avatars/624744152550342666/2b6467cabfc270febe07ee084b86011d.png?size=1024",
-	20000,
-	220000
-);
-
-createAutomate(
-	ten,
-	"Ten",
-	"https://cdn.discordapp.com/avatars/410571285148598282/0cd64e91d12e649de7b1f7456af46951.png?size=1024",
-	20000,
-	220000
-);
-
-createAutomate(
-	rceckie,
-	"RCEckie",
-	"https://cdn.discordapp.com/avatars/467383775165284373/e5384d38bcf73206c132155ba824f500.webp?size=1024",
-	20000,
-	120000
-);
+	);}
+for (const it of list) createAutomate(it.name, it.username ?? it.name, it.pfp, it.range && it.range[0], it.range && it.range[1]);
---
+++ src/list.ts
@@ -0,0 +1,56 @@
+export const list = [
+	{
+		name: "zirqn",
+		username: "furqn",
+		pfp: "https://cdn.discordapp.com/attachments/735321036471402507/744397698576416848/Untitled1352_20200815202904.png"
+	},
+	{
+		name: "florida",
+		username: "FloridaCan",
+		pfp: "https://cdn.discordapp.com/avatars/678405690464665631/e8961af99ba6fb340a69089fb7c31bcb.png?size=2048"
+	},
+	{ 
+		name: "bow",
+		username: "bowknee shape row",
+		pfp: "https://cdn.discordapp.com/avatars/156922894415429632/baf192f377fec106aa19cf856a7ee5f1.png?size=2048"
+	},
+	{
+		name: "rad",
+		username: "Radeline",
+		pfp: "https://cdn.discordapp.com/avatars/630047851011112970/74d044bf2cc38610e241fd8cf187de48.png?size=2048",
+		range: [2000, 50000]
+	},
+	{
+		name: "dae",
+		username: "Daemon_Ignis",
+		pfp: "https://cdn.discordapp.com/avatars/321349427476299777/84c7f789be81a1e49bb248d295fd787a.png?size=2048",
+		range: [3000, 60000]
+	},
+	{
+		name: "zef",
+		username: "unexpected william ░",
+		pfp: "https://cdn.discordapp.com/avatars/413143886702313472/5795319df1a0ff89e1d4d491c9ad2879.webp?size=1024"
+	},
+	{
+		name: "fin",
+		username: "Findex_",
+		pfp: "https://cdn.discordapp.com/avatars/320591753956687882/a_ab412847d943c78188d595c5f50dc04c.gif?size=2048"
+	},
+	{
+		name: "axo",
+		pfp: "https://cdn.discordapp.com/avatars/624744152550342666/2b6467cabfc270febe07ee084b86011d.png?size=1024",
+		range: [20000, 220000]
+	},
+	{
+		name: "ten",
+		username: "Ten",
+		pfp: "https://cdn.discordapp.com/avatars/410571285148598282/0cd64e91d12e649de7b1f7456af46951.png?size=1024",
+		range: [20000, 220000]
+	},
+	{
+		name: "rceckie",
+		username: "RCEckie",
+		pfp: "https://cdn.discordapp.com/avatars/467383775165284373/e5384d38bcf73206c132155ba824f500.webp?size=1024",
+		range: [3000, 60000]
+	}
+] 
